fc88c54ecd30e04c5abdd40715c0117ec39d363e
Alex Dollard <alexdollard141@gmail.com>
2020-02-11 21:03:43 -0600

    created "Dog" model and associated btw "User"
    also moved signup.html out of stylesheets folder

---
+++ app/models/dog.js
@@ -0,0 +1,41 @@
+module.exports = function(sequelize, DataTypes){
+    let Dog = sequelize.define("Dog", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        breed: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        birthday: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        breed: {
+            type: DataTypes.STRING, 
+            allowNull: false, 
+        },
+        sign: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    
+    });
+
+    Dog.associate = function(models) {
+        Dog.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        } )
+    };
+
+    return Dog;
+}
