b2d26a0987481269264eddaecd8f51c7ce13ca6e
Bejigawooga <55114061+Bejigawooga@users.noreply.github.com>
2020-02-13 19:08:18 -0600

    updated routes and cleaned up the server file

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-let express = reqire("express")
+const express = require("express");
+const session = require("express-session");
 
 // Requiring passport as we've configured it
-let passport = require("./app/config/passport");
+const passport = require("./app/config/passport");
 
 // Setting up port and requiring models for syncing
-let PORT = process.env.PORT || 8080;
-let db = require("./models");
+const PORT = process.env.PORT || 8080;
+const db = require("./app/models");
 
 
 // Sets up the Express app to handle data parsing
-let app = express();
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("./public"));
+app.use(express.static("./app/public"));
 
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
@@ -23,13 +24,15 @@ app.use(passport.session());
 
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Require routes folder
-require("./routes")(app);
+require(`./app/routes/api-routes`)(app);
+require(`./app/routes/html-routes`)(app);
+
 
 // Starts the server to begin listening
 db.sequelize.sync({ force: true }).then(function() {
