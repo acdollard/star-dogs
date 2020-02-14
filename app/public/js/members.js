b2e835973197de3275bee740f659afc17d680f37
Alex Dollard <alexdollard141@gmail.com>
2020-02-14 12:17:31 -0600

    Added members.js file and Tailwind modal for adding new dogs
    logout function still isnt working though :(

---
+++ app/public/js/members.js
@@ -0,0 +1,58 @@
+const newDogBtn = $("#newDogBtn");
+const logOutBtn=$("#logOutBtn");
+
+$(document).ready(function() {
+
+logOutBtn.on("click", function(event){
+    event.preventDefault(); 
+    console.log("Flight of the conchords!");
+    $.get("/logout", function(req, res){
+        console.log(res);
+    })
+});
+
+
+
+
+
+
+
+var openmodal = document.querySelectorAll('.modal-open')
+    for (var i = 0; i < openmodal.length; i++) {
+      openmodal[i].addEventListener('click', function(event){
+    	event.preventDefault()
+    	toggleModal()
+      })
+    }
+    
+    const overlay = document.querySelector('.modal-overlay')
+    overlay.addEventListener('click', toggleModal)
+    
+    var closemodal = document.querySelectorAll('.modal-close')
+    for (var i = 0; i < closemodal.length; i++) {
+      closemodal[i].addEventListener('click', toggleModal)
+    }
+    
+    document.onkeydown = function(evt) {
+      evt = evt || window.event
+      var isEscape = false
+      if ("key" in evt) {
+    	isEscape = (evt.key === "Escape" || evt.key === "Esc")
+      } else {
+    	isEscape = (evt.keyCode === 27)
+      }
+      if (isEscape && document.body.classList.contains('modal-active')) {
+    	toggleModal()
+      }
+    };
+    
+    
+    function toggleModal () {
+      const body = document.querySelector('body')
+      const modal = document.querySelector('.modal')
+      modal.classList.toggle('opacity-0')
+      modal.classList.toggle('pointer-events-none')
+      body.classList.toggle('modal-active')
+    }
+
+})
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -108,6 +108,10 @@ module.exports = function(app) {
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
     res.json(req.user);
   });
+
+
+
+
 };
 
 
