f88efe7d5bc91c05bc7963e92e8c08004aaf61b7
Bejigawooga <55114061+Bejigawooga@users.noreply.github.com>
2020-02-15 10:10:25 -0600

    pushing changes made to api routes and signup.js

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -6,7 +6,6 @@
 
 const express = require('express');
 const path = require('path');
-const passport = require("../config/passport")
 
 // Routes
 // =============================================================
@@ -32,7 +31,7 @@ module.exports = function(app) {
   // Get route for members and their dogs
   app.get("/members" , function(req,res){
 
-    res.sendFile(path.join(__dirname + '/../public' + '/stylesheets' + '/members.html'));
+    res.sendFile(path.join(__dirname + '/../public/stylesheets/members.html'));
 
   });
 
@@ -41,14 +40,5 @@ module.exports = function(app) {
 
     res.sendFile(path.join(__dirname + '/../public/stylesheets/signup.html'));
 
-  });
-
-    // Route for logging user out
-  app.get("/logout", function(req, res) {
-    console.log("HELLO:?");
-      req.logout();
-      console.log("Lg out plz");
-      // res.sendFile(path.join(__dirname + '/../public/stylesheets/landingPage.html'));
-      res.redirect("/");
   });
 }
